82f49d62be8b3b0595869eccd56c5ed3afa92a02
TGSDNF <1483314422@qq.com>
2019-08-12 20:17:53 +0800

    aboot

---
+++ aboot/src/main/java/com/wxw/AbootApplication.java
@@ -0,0 +1,13 @@
+package com.wxw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbootApplication.class, args);
+	}
+
+}
